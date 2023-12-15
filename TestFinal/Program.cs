bbedb932f6ef513266eee46540c61b4f6750e52c
LeNguyenThienAn1 <110837183+LeNguyenThienAn1@users.noreply.github.com>
2023-12-15 23:29:25 +0700

    Setup database

--- TestFinal/Program.cs
+++ TestFinal/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TestFinal.Data;
+
 namespace TestFinal
 {
     public class Program
@@ -8,6 +11,10 @@ namespace TestFinal
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options=>
+             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
             var app = builder.Build();
 
