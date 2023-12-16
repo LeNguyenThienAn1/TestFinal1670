faadcc69f4a2ab415c39591c56815dd144cf3d1f
LeNguyenThienAn1 <110837183+LeNguyenThienAn1@users.noreply.github.com>
2023-12-16 11:14:02 +0700

    Design Category

---
+++ TestFinal/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestFinal.Data;
+using TestFinal.Models;
+
+namespace TestFinal.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
--- TestFinal/Data/ApplicationDbContext.cs
+++ TestFinal/Data/ApplicationDbContext.cs
@@ -11,5 +11,14 @@ namespace TestFinal.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuider)
+        {
+            modelBuider.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "History", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Science", DisplayOrder = 3 }
+                );
+        }
     }
 }
